ad2f2f5ad83ecb6c13f069cd4f814676170c18b9
G-McCarten <76708068+G-McCarten@users.noreply.github.com>
2023-02-13 16:54:49 -0500

    fixed bugs

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express =  require("express");
 const app = express();
 const cors = require('cors');
+require("dotenv").config();
 
 app.use(express.json());
 app.use(cors());
@@ -17,10 +18,13 @@ const usersRouter = require('./routes/Users');
 app.use("/auth", usersRouter);
 
 db.sequelize.sync().then(() => {
-    app.listen(3001, () => {
+    app.listen(process.env.PORT || 3001, () => {
         console.log("server running on port 3001");
     }); 
-});
+})
+.catch((err) => {
+    console.log(err)
+})
 
 
 
